4dcf20a1e0ebd0b3d6887c501b0605fa347d2131
yuquanzhou <yuquanzh@usc.edu>
2025-03-10 01:03:08 -0700

    Format and clean routes_api

--- src/routes_api.py
+++ src/routes_api.py
@@ -1,10 +1,10 @@
+"""Call Google API with AIP key to get route information between two locations."""
+
 import concurrent.futures
 import json
-import os
 import re
 from datetime import datetime
 from pathlib import Path
-from typing import Dict, List, Tuple
 
 import pandas as pd
 import polyline
@@ -12,11 +12,11 @@ import pytz
 import requests
 
 _CACHE_FOLDER = "data/route_cache"
-os.makedirs(_CACHE_FOLDER, exist_ok=True)
+Path.makedirs(_CACHE_FOLDER, exist_ok=True)
 
 
 class Location:
-    """This class stores the longitude, latitude, and address of a geographic location.
+    """Stores the longitude, latitude, and address of a geographic location.
 
     Parameters
     ----------
@@ -29,14 +29,15 @@ class Location:
 
     """
 
-    def __init__(self, lon, lat, addr):
+    def __init__(self, lon: float, lat: float, addr: str) -> None:
+        """Initialize the Location object with longitude, latitude, and address."""
         self.lon = lon
         self.lat = lat
         self.addr = addr
 
 
-def parse_dataframe_to_locations(dataframe: pd.DataFrame) -> List[Location]:
-    """Convert a DataFrame containing geometry and address information into a list of Location objects.
+def parse_dataframe_to_locations(dataframe: pd.DataFrame) -> list[Location]:
+    """Convert a DF containing geometry and address information into a list of Location objects.
 
     Parameters
     ----------
@@ -46,25 +47,25 @@ def parse_dataframe_to_locations(dataframe: pd.DataFrame) -> List[Location]:
     Returns
     -------
     locations : list of Location
-        A list of Location objects representing geographic points with longitude, latitude, and address.
+        A list of Location objects representing geographic points with lon, lat, and address.
 
     Notes
     -----
-    The input DataFrame should contain columns for geometry (in a specific format) and address information.
-    The function parses the geometry information to extract longitude, latitude, and uses the provided address.
+    The input DataFrame should contain columns for geometry (in a specific format) and addr info.
+    The function parses the geometry information to extract lon, lat, and uses the provided address.
 
     """
     locations = []
     for i in range(len(dataframe)):
         point = str(dataframe.geometry.iloc[i])
         pattern = r"\((.*?)\)"
-        longLat = re.findall(pattern, point)
+        lon_lat = re.findall(pattern, point)
 
-        if longLat:
-            longLat = longLat[0]
-            splitIndex = longLat.find(" ")
-            longitude = longLat[:splitIndex]
-            latitude = longLat[splitIndex + 1:]
+        if lon_lat:
+            lon_lat = lon_lat[0]
+            split_index = lon_lat.find(" ")
+            longitude = lon_lat[:split_index]
+            latitude = lon_lat[split_index + 1 :]
 
             # Check if the 'address' column exists and use it if available
             address = dataframe.address.iloc[i] if "address" in dataframe.columns else None
@@ -73,7 +74,7 @@ def parse_dataframe_to_locations(dataframe: pd.DataFrame) -> List[Location]:
     return locations
 
 
-def create_departure_time(departure_time: Tuple = None) -> str:
+def create_departure_time(departure_time: tuple[int] | None = None) -> str:
     """Convert a departure time to a standardized UTC format string.
 
     Parameters
@@ -92,22 +93,19 @@ def create_departure_time(departure_time: Tuple = None) -> str:
     Notes
     -----
     The time is localized to the 'US/Pacific' timezone and converted to UTC.
-    If the provided departure_time is invalid, the function will catch the exception and return None.
+    If the provided departure_time is invalid, catch the exception and return None.
 
     """
-    local_tz = pytz.timezone("US/Pacific")
-    departure_time_str = None  # default value for departure_time_str
+    departure_time_str = None
 
     if departure_time is not None:
         try:
-            # Create a naive datetime object
-            naive_departure_time = datetime(*departure_time)
-            # Localize the datetime object to the specific timezone
-            localized_departure_time = local_tz.localize(naive_departure_time)
+            # Create a localized datetime object
+            naive_departure_time = datetime(*departure_time, tzinfo=pytz.timezone("US/Pacific"))
             # Convert to UTC
-            departure_time_utc = localized_departure_time.astimezone(pytz.utc)
+            departure_time_utc = naive_departure_time.astimezone(pytz.utc)
             departure_time_str = departure_time_utc.strftime("%Y-%m-%dT%H:%M:%SZ")
-        except Exception as e:
+        except ValueError as e:
             # Handle exceptions (like invalid departure_time format)
             print(f"Error: {e}")
             return None
@@ -115,8 +113,13 @@ def create_departure_time(departure_time: Tuple = None) -> str:
     return departure_time_str
 
 
-def calculate_route(apikey: str, orig: Location, dest: Location, traffic: str = "TRAFFIC_UNAWARE",
-        departure_time: Tuple = None, ) -> Dict:
+def calculate_route(
+    apikey: str,
+    orig: Location,
+    dest: Location,
+    traffic: str = "TRAFFIC_UNAWARE",
+    departure_time: tuple[int] | None = None,
+) -> dict:
     """Calculate a route between two locations using the Google Routes API.
 
     Parameters
@@ -130,34 +133,44 @@ def calculate_route(apikey: str, orig: Location, dest: Location, traffic: str = 
     traffic : str
         Traffic awareness mode ('TRAFFIC_UNAWARE', 'TRAFFIC_AWARE', 'TRAFFIC_AWARE_OPTIMAL').
     departure_time : tuple
-        A tuple representing the departure time in the format (year, month, day, hour, minute) (default=None).
+        A tuple representing the departure time in the format (year, month, day, hour, minute),
+        default=None.
 
     Returns
     -------
     dict
-        A dictionary containing route information, including duration, distance, and encoded polyline.
+        A dict containing route information, including duration, distance, and encoded polyline.
 
     """
     # set up the headers and body for the request
-    heads = {"X-Goog-Api-Key": apikey, "Content-Type": "application/json",
-        "X-Goog-FieldMask": "routes.distanceMeters,routes.duration,routes.polyline.encodedPolyline", }
+    heads = {
+        "X-Goog-Api-Key": apikey,
+        "Content-Type": "application/json",
+        "X-Goog-FieldMask": "routes.distanceMeters,routes.duration,routes.polyline.encodedPolyline",
+    }
 
-    body = {"origin": {"location": {"latLng": {"latitude": orig.lat, "longitude": orig.lon, }, }, },
-        "destination": {
-            "location": {"latLng": {"latitude": dest.lat, "longitude": dest.lon, }, }, },
-        "travelMode": "DRIVE", "routingPreference": traffic, }
+    body = {
+        "origin": {"location": {"latLng": {"latitude": orig.lat, "longitude": orig.lon}}},
+        "destination": {"location": {"latLng": {"latitude": dest.lat, "longitude": dest.lon}}},
+        "travelMode": "DRIVE",
+        "routingPreference": traffic,
+    }
 
     # add departureTime to the body if it is not None
     if departure_time is not None:
         body["departureTime"] = create_departure_time(departure_time)
 
     # send the request to the Google Routes API
-    response = requests.post("https://routes.googleapis.com/directions/v2:computeRoutes",
-        headers=heads, json=body, )
+    response = requests.post(
+        "https://routes.googleapis.com/directions/v2:computeRoutes",
+        headers=heads,
+        json=body,
+        timeout=10,
+    )
     return response.json()
 
 
-def create_dataframe_from_results(route_results) -> pd.DataFrame:
+def create_dataframe_from_results(route_results: list) -> pd.DataFrame:
     """Create a DataFrame from given route results.
 
     Parameters
@@ -183,35 +196,35 @@ def create_dataframe_from_results(route_results) -> pd.DataFrame:
             if duration is not None:
                 duration = int(duration.rstrip("s"))  # Remove 's' and convert to int
             decoded_polyline = polyline.decode(
-                route.get("polyline", {}).get("encodedPolyline", None), )
+                route.get("polyline", {}).get("encodedPolyline", None),
+            )
 
             # Append a dictionary of the route data to the route_data list
             route_data.append(
-                {"distance": distance, "duration": duration, "polyline": decoded_polyline, }, )
+                {"distance": distance, "duration": duration, "polyline": decoded_polyline},
+            )
 
     # Convert the route_data list of dictionaries to a DataFrame
-    routes_df = pd.DataFrame(route_data)
-
-    return routes_df
+    return pd.DataFrame(route_data)
 
 
-def cache_file_path(origin, destination, departure_time):
+def cache_file_path(origin: Location, destination: Location, departure_time: tuple[int]) -> str:
     """Generate a file path for caching route data based on the origin and destination coordinates.
 
     Parameters
     ----------
     origin : Location
-        A Location object representing the starting point of the route, with latitude and longitude attributes.
+        A Location representing the starting point of the route, with lat and lon attributes.
     destination : Location
-        A Location object representing the destination point of the route, with latitude and longitude attributes.
+        A Location representing the destination point of the route, with lat and lon attributes.
     departure_time : tuple
         The departure time for the route, used to create a unique filename.
 
     Returns
     -------
     str
-        A string representing the path to the cache file, including a unique filename based on the latitude,
-        longitude of origin, destination, and departure time.
+        A string representing the path to the cache file, including a unique filename based on the
+        latitude, longitude of origin, destination, and departure time.
 
     Notes
     -----
@@ -220,11 +233,19 @@ def cache_file_path(origin, destination, departure_time):
 
     """
     departure_time_str = create_departure_time(departure_time)
-    filename = f"route_{origin.lat}_{origin.lon}_to_{destination.lat}_{destination.lon}_{departure_time_str}.json"
-    return os.path.join(_CACHE_FOLDER, filename)
+    filename = (
+        f"route_{origin.lat}_{origin.lon}_to_"
+        f"{destination.lat}_{destination.lon}_{departure_time_str}.json"
+    )
+    return Path.join(_CACHE_FOLDER, filename)
 
 
-def save_to_cache(data, origin, destination, departure_time):
+def save_to_cache(
+    data: dict,
+    origin: Location,
+    destination: Location,
+    departure_time: tuple[int],
+) -> None:
     """Save route data to a cache file.
 
     Parameters
@@ -232,9 +253,9 @@ def save_to_cache(data, origin, destination, departure_time):
     data : dict
         The data to be cached. This would be route information in a dictionary format.
     origin : Location
-        A Location object representing the starting point of the route, with latitude and longitude attributes.
+        A Location representing the starting point of the route, with lat and lon attributes.
     destination : Location
-        A Location object representing the destination point of the route, with latitude and longitude attributes.
+        A Location representing the destination point of the route, with lat and lon attributes.
     departure_time : tuple
         A tuple representing the departure time, used in generating the cache filename.
 
@@ -245,7 +266,8 @@ def save_to_cache(data, origin, destination, departure_time):
 
     Notes
     -----
-    This function saves route data to a cache file to avoid redundant computations or API calls in the future.
+    This function saves route data to a cache file to avoid redundant computations or API calls in
+    the future.
 
     """
     file_path = cache_file_path(origin, destination, departure_time)
@@ -253,32 +275,27 @@ def save_to_cache(data, origin, destination, departure_time):
         json.dump(data, file)
 
 
-def load_from_cache(origin, destination, departure_time):
+def load_from_cache(
+    origin: Location,
+    destination: Location,
+    departure_time: tuple[int],
+) -> dict | None:
     """Load route data from a cache file, if it exists.
 
     Parameters
     ----------
     origin : Location
-        A Location object representing the starting point of the route, with latitude and longitude attributes.
+        A Location representing the starting point of the route, with lat and lon attributes.
     destination : Location
-        A Location object representing the destination point of the route, with latitude and longitude attributes.
+        A Location representing the destination point of the route, with lat and lon attributes.
     departure_time : tuple
         A tuple representing the departure time, used in determining the cache file.
 
     Returns
     -------
     dict or None
-        If a cached file exists for the specified route, the function returns the data in dictionary format.
-        If no cache file exists for the route, the function returns None.
-
-    Notes
-    -----
-    The function first checks if a cache file exists at the generated path using `os.path.exists`. If the file exists,
-    the function opens the file in 'r' (read) mode and loads the JSON data into a Python dictionary, which is then returned.
-
-    If the file does not exist, indicating that the route has not been cached previously, the function returns None.
-    This design allows the calling code to decide how to handle the absence of cached data, such as fetching fresh data
-    if necessary.
+        If a cached exists for the specified route, returns the data in dictionary format.
+        If no cache exists for the route, returns None.
 
     """
     file_path = cache_file_path(origin, destination, departure_time)
@@ -288,8 +305,13 @@ def load_from_cache(origin, destination, departure_time):
     return None
 
 
-def fetch_all_routes(apikey: str, origins: List[Location], destinations: List[Location],
-                     traffic: str, departure_times):
+def fetch_all_routes(
+    apikey: str,
+    origins: list[Location],
+    destinations: list[Location],
+    traffic: str,
+    departure_times: list[tuple[int]],
+) -> list:
     """Fetch route data for multiple origin-destination pairs concurrently, using cache.
 
     Parameters
@@ -312,19 +334,23 @@ def fetch_all_routes(apikey: str, origins: List[Location], destinations: List[Lo
 
     Notes
     -----
-    The function employs a ThreadPoolExecutor for concurrent execution, improving performance for multiple route calculations.
-    It first checks if the route data is available in the cache. If so, it retrieves the data directly from the cache.
-    For routes not available in the cache, it calculates them using the 'calculateRoute' function in a parallel manner.
-    Calculated routes are then cached for future use.
-    The results are sorted and returned in the order corresponding to the input lists of origins and destinations.
-    Exceptions during route calculations are caught and printed, but do not halt the execution of the entire function.
+    The function employs a ThreadPoolExecutor for concurrent execution, improving performance for
+    multiple route calculations.
+
+    It first checks if the route data is available in the cache. If so, it retrieves the data
+    directly from the cache. Otherwise, it calculates them using the 'calculateRoute' function in a
+    parallel manner. Calculated routes are then cached for future use.
+    The results are sorted and returned in the order corresponding to the input lists of origins and
+    destinations. Exceptions during route calculations are caught and printed, but do not halt the
+    execution of the entire function.
 
     """
     results_with_keys = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         future_to_route = {}
         for i, (origin, destination, dep_time) in enumerate(
-                zip(origins, destinations, departure_times, strict=False), ):
+            zip(origins, destinations, departure_times, strict=False),
+        ):
             # Find the data in the cache
             cached_data = load_from_cache(origin, destination, dep_time)
             if cached_data:
@@ -332,8 +358,14 @@ def fetch_all_routes(apikey: str, origins: List[Location], destinations: List[Lo
                 results_with_keys.append((i, cached_data))
             else:
                 # If data is not in the cache, start the task
-                future = executor.submit(calculate_route, apikey, origin, destination, traffic,
-                    dep_time, )
+                future = executor.submit(
+                    calculate_route,
+                    apikey,
+                    origin,
+                    destination,
+                    traffic,
+                    dep_time,
+                )
                 future_to_route[future] = (i, origin, destination, dep_time)
 
         for future in concurrent.futures.as_completed(future_to_route):
@@ -342,9 +374,8 @@ def fetch_all_routes(apikey: str, origins: List[Location], destinations: List[Lo
                 data = future.result()
                 save_to_cache(data, origin, destination, dep_time)
                 results_with_keys.append((index, data))
-            except Exception as exc:
+            except requests.exceptions.RequestException as exc:
                 print(f"Exception for request from {origin} to {destination} at {dep_time}: {exc}")
 
     # Sort the results based on the index key
-    sorted_results = [data for _, data in sorted(results_with_keys, key=lambda x: x[0])]
-    return sorted_results
+    return [data for _, data in sorted(results_with_keys, key=lambda x: x[0])]
