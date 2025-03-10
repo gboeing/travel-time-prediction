f0aa0aeaa766ccfc0445c0914c9ef87a3a7fb693
yuquanzhou <yuquanzh@usc.edu>
2025-03-09 20:26:12 -0700

    Finish preprocess input file.

--- src/constants.py
+++ src/constants.py
@@ -6,9 +6,15 @@ INTERMEDIATE_DIR = "data/intermediate/"
 UCDB_FILE_PATH = INPUT_DIR + "GHS_STAT_UCDB2015MT_GLOBE_R2019A_V1_2.gpkg"
 CA_TRACT_FILE_PATH = INPUT_DIR + "tl_2022_06_tract/tl_2022_06_tract.shp"
 UBER_TRACT_FILE_PATH = INPUT_DIR + "los_angeles_censustracts.json"
+UBER_TRAVEL_TIME_FILE_PATH = INPUT_DIR + "los_angeles-censustracts-2020-1-All-HourlyAggregate.csv"
 
 LA_CLIP_CONVEX_FILE_PATH = INTERMEDIATE_DIR + "la_clip_convex.shp"
 LA_CLIP_CONVEX_NETWORK_GML_FILE_PATH = INTERMEDIATE_DIR + "la_clip_convex_strong_network.graphml"
 CONVEX_STRONGLY_ATTRIBUTES_FILE_PATH = (
     INTERMEDIATE_DIR + "nodes_candidate_convex_strongly_attributes.csv"
 )
+SAMPLED_OD_FILE_PATH = INTERMEDIATE_DIR + "OD_5m_strong.csv"
+SAMPLED_OD_ALL_UBER_FILE_PATH = INTERMEDIATE_DIR + "OD_pairs_uber_all_strongly_119w.csv"
+SAMPLED_OD_UBER_1m_FILE_PATH = INTERMEDIATE_DIR + "OD_pairs_uber_1m_strongly.csv"
+NOT_SAMPLED_OD_UBER_1m_FILE_PATH = INTERMEDIATE_DIR + "OD_pairs_uber_remains_strongly_19w.csv"
+SAMPLED_OD_NOT_UBER_1m_FILE_PATH = INTERMEDIATE_DIR + "OD_pairs_uber_4m_remains_strongly.csv"
--- src/preprocess_input_data.py
+++ src/preprocess_input_data.py
@@ -2,6 +2,7 @@
 
 import geopandas as gpd
 import osmnx as ox
+import pandas as pd
 
 import constants
 
@@ -113,8 +114,67 @@ def get_annotated_graph(
     return gdf_proj_tract_uber
 
 
+def sample_od_pairs(gdf_proj_tract_uber: gpd.GeoDataFrame, sample_size: int) -> gpd.GeoDataFrame:
+    """Sample OD pairs intersected with uber movement data from the given GeoDataFrame.
+
+    Parameters
+    ----------
+    gdf_proj_tract_uber: gpd.GeoDataFrame
+        Geo Dataframe with census and uber tracts ID attached.
+    sample_size: int
+        sample size of OD pairs
+
+    Returns
+    -------
+    Sampled ID pairs that in the uber movement.
+
+    """
+    origin = gdf_proj_tract_uber.sample(sample_size, random_state=123, replace=True).copy()
+    destination = gdf_proj_tract_uber.sample(sample_size, random_state=321, replace=True).copy()
+    origin.reset_index(drop=True)
+    destination.reset_index(drop=True)
+
+    origin_od = origin[["osmid", "y", "x", "GEOID", "MOVEMENT_ID"]]
+    origin_od.columns = ["oid", "oy", "ox", "oGEOID", "oMOVEMENT_ID"]
+    destination_od = destination[["osmid", "y", "x", "GEOID", "MOVEMENT_ID"]]
+    destination_od.columns = ["did", "dy", "dx", "dGEOID", "dMOVEMENT_ID"]
+    od_pairs = pd.concat([origin_od, destination_od], sort=False, axis=1)
+
+    od_pairs = od_pairs.drop(od_pairs[od_pairs["oid"] == od_pairs["did"]].index)
+    constants.to_csv(constants.SAMPLED_OD_FILE_PATH)
+
+    # Subset OD pairs that have reference to uber movement 2020
+    od_pairs["oMOVEMENT_ID"] = od_pairs["oMOVEMENT_ID"].astype(float)
+    od_pairs["dMOVEMENT_ID"] = od_pairs["dMOVEMENT_ID"].astype(float)
+    od_pairs["uber_OD"] = list(zip(od_pairs.oMOVEMENT_ID, od_pairs.dMOVEMENT_ID, strict=False))
+
+    uber_2020_travel_time = pd.read_csv(
+        constants.UBER_TRAVEL_TIME_FILE_PATH,
+        dtype={"sourceid": float, "dstid": float},
+    )
+    # Select the OD pairs that have reference in uber movement 2020
+    unique_od = set(zip(uber_2020_travel_time.sourceid, uber_2020_travel_time.dstid, strict=False))
+    od_pairs_uber = od_pairs[od_pairs["uber_OD"].isin(unique_od)]
+    # Save all the sampled OD pairs that have a reference in uber movement 2020 to csv
+    od_pairs_uber.to_csv(constants.SAMPLED_OD_ALL_UBER_FILE_PATH)
+
+    # sample one million OD pairs out of 1,197,651 OD pairs that have reference with uber movement
+    od_pairs_uber_1m = od_pairs_uber.sample(1000000, random_state=123).copy()
+    od_pairs_uber_1m.to_csv(constants.SAMPLED_OD_UBER_1m_FILE_PATH)
+
+    uber_pairs_remains = od_pairs_uber[~od_pairs_uber.index.isin(od_pairs_uber_1m.index)]
+    # Save all not sampled OD pairs that have a reference in uber movement 2020 to csv
+    uber_pairs_remains.to_csv(constants.NOT_SAMPLED_OD_UBER_1m_FILE_PATH)
+
+    od_pairs_remains = od_pairs[~od_pairs.index.isin(od_pairs_uber_1m.index)]
+    od_pairs_remains.to_csv(constants.SAMPLED_OD_NOT_UBER_1m_FILE_PATH)
+
+    return od_pairs_uber_1m
+
+
 if __name__ == "__main__":
     la_ucdb, la_tract, ca_tract, uber_tract = get_gdf_inputs()
     la_convex = create_convex_hull(la_ucdb, la_tract)
     gdf_proj_tract = get_annotated_graph(la_convex, ca_tract, uber_tract)
-    print(gdf_proj_tract.head())
+    sampled_uber_od_pairs = sample_od_pairs(gdf_proj_tract, 5000000)
+    print(sampled_uber_od_pairs)
